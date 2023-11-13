30f4763f0fad1df84313783b3db1cf5200ca6233
larry8900 <quickexchange488@gmail.com>
2023-11-14 00:05:14 +0100

    01_Day

---
+++ 01_Day_Introduction/datatypes.js
@@ -0,0 +1,49 @@
+// Exercise 5
+
+let firstName = 'Larry';
+let developer = true ;
+let notDefined = undefined ;
+let isNull = null;
+
+console.log(typeof(firstName));
+console.log(typeof(developer));
+console.log(typeof(notDefined));
+console.log(typeof(isNull));
+
+
+//Exercise 6
+
+let names;
+let school;
+let bool;
+let notDefineed;
+
+// Exercise 7 
+
+let stack = 'frontend';
+let age = 10;
+let male = true;
+let likes = undefined;
+
+// Exercise 8
+
+let firstname = 'Ridwan';
+let lastname = 'Adesina';
+let maritalstatus = 'single';
+let country = 'Nigeria';
+let ages = 12;
+
+
+// Exercise 9
+
+let first_Name = "Ridwan", last_name = "Adesina", marital_status = "single", country_ = "nigeria", age_ = 20;
+
+
+
+// Exercise 10
+
+const myAge = 12;
+const yourAge = 29;
+
+console.log(`i am ${myAge} years old`);
+console.log(`You are ${yourAge} years old`);
---
+++ 01_Day_Introduction/variable.js
@@ -0,0 +1,7 @@
+// Exercise 4
+
+let name = 'Larry';
+let developer = true ;
+let notDefined = undefined ;
+let isNull = null;
+
