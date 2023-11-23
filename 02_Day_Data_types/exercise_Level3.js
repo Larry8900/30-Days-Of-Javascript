f167c4d45e7bb2dd3a332e4f6caae1bd2729eaa0
larry8900 <quickexchange488@gmail.com>
2023-11-23 02:42:57 +0100

    02_Day

--- 02_Day_Data_types/exercise_Level3.js
+++ 02_Day_Data_types/exercise_Level3.js
@@ -25,7 +25,12 @@ console.log(repeatedArray.length); // 3
 
 const sentences = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
 const nonWordCharacters = (sentences.replace(/\W/g,' '));
-console.log(nonWordCharacters.trim(''));
+console.log(nonWordCharacters.split(','));
+
+// i'm stucked here
+for(let i = 0; i< nonWordCharacters.length; i++){
+    console.log(nonWordCharacters.match(`${i}`), i)
+}
 
 //4
 const word = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
